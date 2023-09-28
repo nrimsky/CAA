156a1d048eb6f44999d30aa6b39d58b7219fd930
nrimsky <ninarimsky@gmail.com>
2023-09-28 16:27:06 +0000

    15 - 16

--- scripts/compare_prompting.sh
+++ scripts/compare_prompting.sh
@@ -14,12 +14,12 @@ fi
 echo "Generating datasets"
 python make_datasets.py --generate_test_split 0.8 --anthropic_custom_split 0.6 --n_datapoints 1200
 
-# Generate steering vector for layer 15
+# Generate steering vector for layer 16
 echo "Generating steering vectors"
-python generate_vectors.py --layers 15
+python generate_vectors.py --layers 16
 
 # In-distribution tests
 echo "In-distribution A/B question tests"
-python prompting_with_steering.py --type in_distribution --layers 15 --multipliers -2.0 -1.5 -1 -0.5 0 0.5 1 1.5 2.0 --few_shot none
-python prompting_with_steering.py --type in_distribution --layers 15 --multipliers -2.0 -1.5 -1 -0.5 0 0.5 1 1.5 2.0 --few_shot positive
-python prompting_with_steering.py --type in_distribution --layers 15 --multipliers -2.0 -1.5 -1 -0.5 0 0.5 1 1.5 2.0 --few_shot negative
+python prompting_with_steering.py --type in_distribution --layers 16 --multipliers -2.0 -1.5 -1 -0.5 0 0.5 1 1.5 2.0 --few_shot none
+python prompting_with_steering.py --type in_distribution --layers 16 --multipliers -2.0 -1.5 -1 -0.5 0 0.5 1 1.5 2.0 --few_shot positive
+python prompting_with_steering.py --type in_distribution --layers 16 --multipliers -2.0 -1.5 -1 -0.5 0 0.5 1 1.5 2.0 --few_shot negative
--- scripts/truthful_qa.sh
+++ scripts/truthful_qa.sh
@@ -14,12 +14,12 @@ fi
 echo "Generating datasets"
 python make_datasets.py --generate_test_split 0.8 --anthropic_custom_split 0.6 --n_datapoints 1200
 
-# Generate steering vector for layer 15
+# Generate steering vector for layer 16
 echo "Generating steering vectors"
-python generate_vectors.py --layers 15
+python generate_vectors.py --layers 16
 
 # TruthfulQA tests
 echo "TruthfulQA A/B question tests"
-python prompting_with_steering.py --type truthful_qa --layers 15 --multipliers -2.0 -1.5 -1 -0.5 0 0.5 1 1.5 2.0 --few_shot none
+python prompting_with_steering.py --type truthful_qa --layers 16 --multipliers -2.0 -1.5 -1 -0.5 0 0.5 1 1.5 2.0 --few_shot none
 
 
