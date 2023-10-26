98dfa3365dd133b1bb08dc116d10b5745aa0852b
nrimsky <ninarimsky@gmail.com>
2023-10-26 16:52:31 +0000

    Compare Prompting

--- scripts/compare_prompting.sh
+++ scripts/compare_prompting.sh
@@ -12,10 +12,10 @@ fi
 
 # In-distribution tests
 echo "In-distribution A/B question tests"
-python prompting_with_steering.py --type in_distribution --layers 16 --multipliers -1.5 -1 -0.5 0 0.5 1 1.5 --few_shot none
-python prompting_with_steering.py --type in_distribution --layers 16 --multipliers -1.5 -1 -0.5 0 0.5 1 1.5 --few_shot positive
-python prompting_with_steering.py --type in_distribution --layers 16 --multipliers -1.5 -1 -0.5 0 0.5 1 1.5 --few_shot negative
+python prompting_with_steering.py --type in_distribution --layers 15 --multipliers -1.5 -1 -0.5 0 0.5 1 1.5 --few_shot none
+python prompting_with_steering.py --type in_distribution --layers 15 --multipliers -1.5 -1 -0.5 0 0.5 1 1.5 --few_shot positive
+python prompting_with_steering.py --type in_distribution --layers 15 --multipliers -1.5 -1 -0.5 0 0.5 1 1.5 --few_shot negative
 
 # Plot results
 echo "Plotting results"
-python plot_results.py --type in_distribution --layers 16 --multipliers -1.5 -1 -0.5 0 0.5 1 1.5
+python plot_results.py --type in_distribution --layers 15 --multipliers -1.5 -1 -0.5 0 0.5 1 1.5
