3a62dfbe422e034452524cbced5c68af24326a3f
nrimsky <ninarimsky@gmail.com>
2023-10-28 17:36:50 +0100

    Update

--- analysis/plot_as_dots.py
+++ analysis/plot_as_dots.py
@@ -15,9 +15,8 @@ def plot_as_dots(filename):
     else:
         print("Not 3 columns - skipping")
         return
-
-    # Create the updated plot with the specified changes
-    plt.figure(figsize=(12, 8))
+    
+    plt.figure(figsize=(max(len(data)//12, 2), 6))
 
     # Create a color map for the unique multipliers
     unique_multipliers = sorted(data["Multiplier"].unique())
@@ -27,7 +26,7 @@ def plot_as_dots(filename):
     # Plot points and lines for each category
     for category in data["Category"].unique():
         category_data = data[data["Category"] == category]
-        for idx, row in category_data.iterrows():
+        for _, row in category_data.iterrows():
             plt.scatter(category, row["Probability"], color=color_map[row["Multiplier"]], s=100, marker='o')
         plt.plot([category]*len(category_data), category_data["Probability"], linestyle='-', color='grey', alpha=0.5)
 
@@ -35,16 +34,11 @@ def plot_as_dots(filename):
     for multiplier, color in color_map.items():
         plt.plot([], [], 'o', color=color, label=f"{multiplier}", markersize=8)
 
-    plt.legend(loc="upper left", title="Multipliers")
+    plt.legend(title="Multipliers")
 
-    # Set plot details
-    # plt.title("Probability of Correct Answer on TruthfulQA Category by Multiplier")
-    # plt.xlabel("Category")
-    # plt.ylabel("Probability of Correct Answer")
-    plt.xticks(rotation=45)
-    # Make sure every x tick appears
     # if x axis is categorical
     if data["Category"].dtype == object:
+        plt.xticks(rotation=80)
         plt.xticks(np.arange(len(data["Category"].unique())), data["Category"].unique())
     # if x axis is numerical, plot corresponding to correct numbers
     else:
