8ac22f3791b2383299e61d035847a46ea4b1c5ab
nrimsky <ninarimsky@gmail.com>
2024-02-03 10:48:04 +0000

    Update

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
-python prompting_with_steering.py --layers $(seq 0 35) --multipliers -1 0 1 --type ab --model_size "13b"
+python prompting_with_steering.py --layers $(seq 0 31) --multipliers -1 0 1 --type ab --behaviors hallucination --overwrite
+
+
 python prompting_with_steering.py --layers 13 --multipliers -1 -0.5 0 0.5 1 --type ab --model_size "7b" --behaviors hallucination --overwrite
 python prompting_with_steering.py --layers 13 --multipliers -1 -0.5 0 0.5 1 --type ab --model_size "7b"
 python prompting_with_steering.py --layers 13 --multipliers -1 -0.5 0 0.5 1 --type ab --model_size "7b" --system_prompt pos --overwrite
