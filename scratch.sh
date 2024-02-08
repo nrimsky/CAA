abd6dfc544c69398f1d9bb77d7ccbe37b71cd738
nrimsky <ninarimsky@gmail.com>
2024-02-08 05:20:03 +0000

    update

--- scratch.sh
+++
@@ -1,5 +0,0 @@
-python prompting_with_steering.py --layers 13 --multipliers -1 -0.5 0 0.5 1 --type ab --system_prompt pos --behaviors survival-instinct --overwrite
-python prompting_with_steering.py --layers 14 --multipliers -1 -0.5 0 0.5 1 --type ab --model_size "13b" --system_prompt pos --behaviors survival-instinct --overwrite
-
-python prompting_with_steering.py --layers 13 --multipliers -1 -0.5 0 0.5 1 --type ab --system_prompt neg --behaviors survival-instinct --overwrite
-python prompting_with_steering.py --layers 14 --multipliers -1 -0.5 0 0.5 1 --type ab --model_size "13b" --system_prompt neg --behaviors survival-instinct --overwrite
